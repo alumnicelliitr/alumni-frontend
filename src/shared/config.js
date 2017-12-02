8bdd178300440bd27c80c0182679585712e3126e
Agrim Mittal <agrimmittal97@gmail.com>
2017-12-02 15:31:09 +0530

    Basic Structure

---
+++ src/shared/config.js
@@ -0,0 +1,8 @@
+export const WEB_PORT = process.env.PORT || 8000
+export const STATIC_PATH = '/static'
+export const APP_NAME = 'Alumni Network IITR'
+
+export const WDS_PORT = 7000
+
+export const APP_CONTAINER_CLASS = 'root'
+export const APP_CONTAINER_SELECTOR = `.${APP_CONTAINER_CLASS}`
