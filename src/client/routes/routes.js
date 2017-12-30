d352dbc44329deee6f7c4bbbd2bc8c1bc8911955
kritisingh1 <kritisingh1.ks@gmail.com>
2017-12-30 10:51:01 +0530

    added routes

--- src/client/routes/routes.js
+++ src/client/routes/routes.js
@@ -2,6 +2,20 @@ import Base from '../Components/Pages/Base'
 
 import WelcomeContainer from '../Containers/WelcomeContainer'
 import NotFoundContainer from '../Containers/NotFoundContainer'
+import Vision from '../Containers/Vision'
+import Dora from '../Containers/Dora'
+import Team from '../Containers/Team'
+import SocialLinks from '../Containers/SocialLinks'
+import UpcomingEvents from '../Containers/UpcomingEvents'
+import RecentEvents from '../Containers/RecentEvents'
+import MouGeneral from '../Containers/MouGeneral'
+import MouParticular from '../Containers/MouParticular'
+import Initiatives from '../Containers/Initiatives'
+import Alum from '../Containers/Alum'
+import Story from '../Containers/Story'
+import Donate from '../Containers/Donate'
+import ContactUs from '../Containers/ContactUs'
+import WriteToUs from '../Containers/WriteToUs'
 
 import * as rC from './routeConstants'
 
@@ -11,6 +25,62 @@ const routes = {
     {
       path: rC.WELCOME_ROUTE,
       component: WelcomeContainer,
+    },
+    {
+      path: rC.VISION_ROUTE,
+      component: Vision,
+    },
+    {
+      path: rC.DORA_ROUTE,
+      component: Dora,
+    },
+    {
+      path: rC.TEAM_ROUTE,
+      component: Team,
+    },
+    {
+      path: rC.SOCIAL_LINKS_ROUTE,
+      component: SocialLinks,
+    },
+    {
+      path: rC.UPCOMING_EVENTS_ROUTE,
+      component: UpcomingEvents,
+    },
+    {
+      path: rC.RECENT_EVENTS_ROUTE,
+      component: RecentEvents,
+    },
+    {
+      path: rC.MOU_GENERAL_ROUTE,
+      component: MouGeneral,
+    },
+    {
+      path: rC.MOU_PARTICULAR_ROUTE,
+      component: MouParticular,
+    },
+    {
+      path: rC.INITIATIVES_ROUTE,
+      component: Initiatives,
+    },
+    {
+      path: rC.ALUM_ROUTE,
+      component: Alum,
+    },
+    {
+      path: rC.STORY_ROUTE,
+      component: Story,
+    },
+    {
+      path: rC.DONATE_ROUTE,
+      component: Donate,
+    },
+    {
+      path: rC.CONTACT_US_ROUTE,
+      component: ContactUs,
+    },
+    {
+      path: rC.WRITE_TO_US_ROUTE,
+      component: WriteToUs,
     },
     {
       path: '*',
