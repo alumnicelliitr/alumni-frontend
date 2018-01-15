4bd233eb63e50c0616bc01d6eaee6693242eead5
kritisingh1 <kritisingh1.ks@gmail.com>
2018-01-15 23:28:07 +0530

    updated the pages

---
+++ src/client/Containers/Initiatives.jsx
@@ -0,0 +1,136 @@
+import React from 'react'
+import { Link } from 'react-router'
+
+class Initiatives extends React.Component {
+  render() {
+    return (
+    	<div classNameName="initiatives">
+    	<div className="navbar">
+          <div className="loginbutton">Login</div>
+            <div className="links-div">
+                <div className="nav-links"><a href="about/vision">About</a></div>
+                <div className="nav-links"><a href="/events/recent">Events</a></div>
+                <div className="nav-links"><a href="/mou">MOUs</a></div>
+                <div className="nav-links"><a href="/initiatives">Initiatives</a></div>
+                <div className="nav-links"><a href="/awards">Awards</a></div>
+                <div className="nav-links"><a href="/donate">Donate</a></div>
+                <div className="nav-links"><a href="/contact">Contact</a></div>
+          </div>
+      </div>
+      <div className="separator"></div>
+      <div className="initiatives-samp-section">
+      <div className="in-samp-heading">
+          <div className="in-samp-heading-main">
+              Student Alumni
+          </div>
+          <div className="in-samp-heading-sub">
+              Mentorship Program
+          </div>
+      </div>
+      <div className="in-samp-text">
+      Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero erovcumsan 
+      </div>
+      <div className="in-samp-register-button">
+          Register
+      </div>
+      <div className="in-samp-placeholder">
+          
+      </div>
+      </div>
+      <div className="initiatives-connect-section">
+      <div className="connect-heading">
+          <div className="connect-heading-main">
+              Connect On
+          </div>
+          <div className="connect-heading-sub">
+              Request
+          </div>
+      </div>
+      <div className="connect-text">
+          Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolorin hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero erovcumsan 
+      </div>
+      <div className="connect-section-placeholder">
+          
+      </div>
+      </div>
+      <div className="crowdfunding-section">
+        <div className="crowdfunding-section-heading">
+            <div className="crowdfunding-section-heading-main">
+                Crowd
+            </div>
+            <div className="crowdfunding-section-heading-sub">
+                Funding
+            </div>
+        </div>
+        <div className="crowdfunding-section-placeholder">
+              
+        </div>
+        <div className="crowdfunding-section-text">
+              Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diamnonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero erovcumsan 
+        </div>
+        <div className="crowdfunding-visit-button">
+              Visit
+        </div>
+      </div>
+      <div className="video-lecture-section">
+          <div className="video-repo-section">
+              <div className="video-repo-section-heading">
+                  <div className="video-repo-heading-main">
+                      Video
+                  </div>
+                  <div className="video-repo-heading-sub">
+                      Repository
+                  </div>
+              </div>
+              <div className="video-repo-section-text">
+                  
+              </div>
+          </div>
+          <div className="lecture-series-section">
+              <div className="lecture-series-section-heading">
+                  <div className="lecture-series-heading-main">
+                      Lecture
+                  </div>
+                  <div className="lecture-series-heading-sub">
+                      Series
+                  </div>
+              </div>
+          </div>
+          <div className="initiatives-video-window">
+              
+          </div>
+          <div className="initiatives-video-window-text">
+              
+          </div>
+          <div className="youtube-channel-button">
+              YouTube Channel
+          </div>
+      </div>
+      <div className="webinars">
+          <div className="webinars-heading">
+              
+          </div>
+          <div className="webinars-text">
+              
+          </div>
+          <div className="webinars-placeholder">
+              
+          </div>
+      </div>
+      <div className="streaming">
+          <div className="live-streaming-heading">
+              
+          </div>
+          <div className="live-streaming-text">
+              
+          </div>
+          <div className="watch-button">
+              Watch
+          </div>
+      </div>
+      </div>
+    )
+  }
+}
+
+export default Initiatives
