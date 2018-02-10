4955ee084322ea6cec747b6af0d27bf6a4b69e92
psinghal20 <psinghal20@gmail.com>
2018-02-11 03:24:26 +0530

    refactor: rewrite navbar structure

--- src/client/Components/navbar.jsx
+++ src/client/Components/navbar.jsx
@@ -4,18 +4,21 @@ import '../styles/navbar.css';
 export default class Navbar extends React.Component {
     render() {
         return (
-                  <div className="navbar">
-                    <div className="loginbutton">Login</div>
-                    <div className="links-div">
-                      <div className="nav-links"><a href="about/vision">About</a></div>
-                      <div className="nav-links"><a href="/events/recent">Events</a></div>
-                      <div className="nav-links"><a href="/mou">MOUs</a></div>
-                      <div className="nav-links"><a href="/initiatives">Initiatives</a></div>
-                      <div className="nav-links"><a href="/awards">Awards</a></div>
-                      <div className="nav-links"><a href="/donate">Donate</a></div>
-                      <div className="nav-links"><a href="/contact">Contact</a></div>
+                    <div className="navbar">
+                      <div className="navbar-image"><img src=""/></div>
+                      <div className="navbar-container">
+                        <div className="navbar-links-container">
+                          <a href="/about/vision" className="nav-links">About</a>
+                          <a href="/events/recent" className="nav-links">Events</a>
+                          <a href="/mou" className="nav-links">MOUs</a>
+                          <a href="/initiatives" className="nav-links">Initiatives</a>
+                          <a href="/awards" className="nav-links">Awards</a>
+                          <a href="/donate" className="nav-links">Donate</a>
+                          <a href="/contact" className="nav-links">Contact</a>
+                        </div>
+                        <div className="loginbutton">Login</div>
+                      </div>
                     </div>
-                  </div>
                 );
     }
 }
