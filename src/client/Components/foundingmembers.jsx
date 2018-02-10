fefebae026d47049906f2561f9833e18a4bbf359
psinghal20 <psinghal20@gmail.com>
2018-02-10 15:51:31 +0530

    feat: add foundingmembers and testimonial components

---
+++ src/client/Components/foundingmembers.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+export default class FoundingMembers extends React.Component {
+  render() {
+    return (
+        <div className="founding-members">
+          <div className="founding-members-card">
+            <img src=""/>
+            <div className="founding-members-card-content">
+              <div className="members-info-list">
+                <ul>
+                  <li><span>Name</span></li>
+                  <li><span>Batch</span></li>
+                  <li><span>Branch</span></li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      );
+  }
+}
