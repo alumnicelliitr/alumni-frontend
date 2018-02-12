bc4567b946a88ab448d693b542ee3a3590b5c5b8
psinghal20 <psinghal20@gmail.com>
2018-02-12 23:52:36 +0530

    feat: add events tab component and styles

--- src/client/Containers/RecentEvents.jsx
+++ src/client/Containers/RecentEvents.jsx
@@ -1,58 +1,30 @@
 import React from 'react'
 import { Link } from 'react-router'
 import Navbar from '../Components/navbar.jsx';
+import RecentEvent from '../Components/recentEvent.jsx';
+import UpcomingEvent from '../Components/upcomingEvent.jsx';
+
+import '../styles/events.css';
 
 class RecentEvents extends React.Component {
   render() {
     return (
     	<div className="recentevents">
     		<Navbar/>
-      <div className="separator">
-
-      </div>
-      <div className="content-recent-events">
-        <div className="upcoming-events-inactive">
-          <div className="upcoming-events-inactive-heading">
-            <div className="upcoming-events-inactive-heading-main">
-              Upcoming
+        <div className="content-recent-events container">
+          <div className="row">
+            <div className="tabs-list-container">
+              <div className="col s8 container">
+                <ul className="tabs tab-menu">
+                  <li className="tab col s6 "><a href="#upcoming" className="grey-text tab-link">Upcoming <br/>Events</a></li>
+                  <li className="tab col s6"><a href="#recent" className="grey-text tab-link">Recent<br/>Events</a></li>
+                </ul>
+              </div>
             </div>
-            <div className="upcoming-events-inactive-heading-sub">
-              Events
-            </div>
+            <div id="upcoming" className="events-containers"><UpcomingEvent/></div>
+            <div id="recent" className="events-containers"><RecentEvent/></div>
           </div>
         </div>
-        <div className="recent-events-active">
-          <div className="recent-events-active-heading">
-            <div className="recent-events-active-heading-main">
-              Recent
-            </div>
-            <div className="recent-events-active-heading-sub">
-              Events
-            </div>
-          </div>
-          <div className="recent-events-active-content">
-            <div className="event-name">
-              Global Alumni Meet
-            </div>
-            <div className="event-date">
-              <div className="calender-icon">
-                <img src="calender-icon-red.svg" />
-              </div>
-              <div className="calender-date">
-                23 March 2018
-              </div>
-            </div>
-            <div className="event-location">
-              <div className="location-icon">
-                <img src="location-icon-red.svg" />
-              </div>
-              <div className="location-text">
-                New Delhi
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
       </div>
     )
   }
