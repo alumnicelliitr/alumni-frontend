c9ec3a71cba9e7d63953b13ceac464e52621b254
psinghal20 <psinghal20@gmail.com>
2018-02-12 23:56:16 +0530

    feat: add contact container and styles

--- src/client/Containers/WriteToUs.jsx
+++ src/client/Containers/WriteToUs.jsx
@@ -1,71 +1,31 @@
 import React from 'react'
 import { Link } from 'react-router'
+import Navbar from '../Components/navbar.jsx';
+import ContactUs from '../Components/contactUs.jsx';
 
-class WriteToUs extends React.Component {
+import '../styles/contact.css';
+class Contact extends React.Component {
   render() {
     return (
-  <div className="writetous">
-      <div className="navbar">
-          <div className="loginbutton">Login</div>
-            <div className="links-div">
-          <div className="nav-links"><a href="#about">About</a></div>
-              <div className="nav-links"><a href="#events">Events</a></div>
-                <div className="nav-links"><a href="#mou">MOUs</a></div>
-                <div className="nav-links"><a href="#initiatives">Initiatives</a></div>
-                <div className="nav-links"><a href="#awards">Awards</a></div>
-                <div className="nav-links"><a href="#donate">Donate</a></div>
-                <div className="nav-links"><a href="#contact">Contact</a></div>
-          </div>
+  <div className="contact">
+    <Navbar/>
+    <div className="contact-container">
+      <div className="row">
+        <div className="tabs-list-container">
+        <div className="col s8 container">
+          <ul className="tabs tab-menu">
+            <li className="tab col s6 "><a href="#contact-us" className="grey-text tab-link">Contact Us</a></li>
+            <li className="tab col s6"><a href="#write-to-us" className="grey-text tab-link">Write To Us</a></li>
+          </ul>
+        </div>
+        </div>
+        <div id="contact-us" className="about-containers"><ContactUs/></div>
+        <div id="write-to-us" className="about-containers">write to us</div>
       </div>
-    <div className="separator">
-      
-    </div>
-    <div className="content-write-to-us">
-    <div className="write-to-us-active">
-      <div className="write-to-us-active-content">
-        <div className="write-to-us-active-content-heading">
-          <div className="write-to-us-active-content-heading-main">
-            Write
-          </div>
-          <div className="write-to-us-active-content-heading-sub">
-            To Us
-          </div>
-        </div>
-        <div className="write-to-us-active-content-email-form">
-          <div className="write-to-us-active-content-email-form-heading">
-            Enter mail ID
-          </div>
-          <div className="write-to-us-active-content-email-form-content">
-            <input type="text" />
-          </div>
-        </div>
-        <div className="write-to-us-active-content-comments-form">
-          <div className="write-to-us-active-content-comments-form-heading">
-            Add comments
-          </div>
-          <div className="write-to-us-active-content-comments-form-content">
-            <input type="text" />
-          </div>
-        </div>
-        <button className="write-to-us-submit-button">
-          Submit
-        </button>
-      </div>
-    </div>
-    <div className="contact-us-inactive">
-      <div className="contact-us-inactive-heading">
-        <div className="contact-us-inactive-heading-main">
-          Contact
-        </div>
-        <div className="contact-us-inactive-heading-sub">
-          Us
-        </div>
-      </div>
-    </div>
     </div>
   </div>
     )
   }
 }
 
-export default WriteToUs
+export default Contact
