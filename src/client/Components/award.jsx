3fe8ef790720cef0e44876e552ff688e0fbb0d58
kritisingh1 <kritisingh1.ks@gmail.com>
2018-02-13 20:26:18 +0530

    added remaining code

--- src/client/Components/award.jsx
+++ src/client/Components/award.jsx
@@ -1,18 +1,19 @@
 import React from 'react';
 import '../styles/award.css'
 
-<div>
+export default class Award extends React.Component {
+  render() {
+    return (
+
+	<div>
 	<div className="award-name">Encore Awards</div>
 	<div className="award-description">
-		Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed di
-am nonummy nibh euismod tincidunt ut laoreet dolore magna 
-aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nost
-rud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex 
-ea commodo consequat. Duis autem vel eum iriure dolor in hen
-drerit in vulputate velit esse molestie consequat, vel illum dolor
-e eu feugiat nulla facilisis at vero erovcumsan 
+		Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero erovcumsan 
 	</div>
 	<div className="award-pic">
 
 	</div>
-</div>
+	</div>
+		);
+	}
+}
