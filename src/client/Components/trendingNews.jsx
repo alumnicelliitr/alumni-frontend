de0c599493064fb1893bd9f91d2197fd0621282e
psinghal20 <psinghal20@gmail.com>
2018-03-12 23:32:55 +0530

    feat: add carousel nav buttons

--- src/client/Components/trendingNews.jsx
+++ src/client/Components/trendingNews.jsx
@@ -4,12 +4,31 @@ import '../styles/trendingNews.css';
 export default class TrendingNews extends React.Component {
   componentDidMount(){
     $('.carousel.carousel-slider').carousel({fullWidth: true});
+    $('.moveNextCarousel').click(function(e){
+      e.preventDefault();
+      e.stopPropagation();
+      $('.carousel').carousel('next');
+   });
+
+   $('.movePrevCarousel').click(function(e){
+      e.preventDefault();
+      e.stopPropagation();
+      $('.carousel').carousel('prev');
+   });
   }
 
   render() {
     return (
       <div className="trending-news">
         <div className="carousel carousel-slider center" data-indicators="true">
+          <div className="carousel-fixed-item center middle-indicator">
+            <div className="left">
+              <a className="movePrevCarousel middle-indicator-text waves-effect waves-light content-indicator"><i className="material-icons left  middle-indicator-text">chevron_left</i></a>
+            </div>
+            <div className="right">
+              <a className=" moveNextCarousel middle-indicator-text waves-effect waves-light content-indicator"><i className="material-icons right middle-indicator-text">chevron_right</i></a>
+            </div>
+          </div>
           <a className="carousel-item carousel-links" href="#one!">
             <img src="/static/images/grey.jpg"/>
             <div className="image-heading"><h1>Some News</h1><h2>Tagline</h2></div>
