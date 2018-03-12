96eff086bcd97b8cab19b9b255c18431ddee142c
psinghal20 <psinghal20@gmail.com>
2018-03-12 23:30:25 +0530

    feat: introduce React-router Links in place of anchor tags

--- src/client/Components/navbar.jsx
+++ src/client/Components/navbar.jsx
@@ -1,24 +1,25 @@
 import React from 'react';
+import { Link } from 'react-router';
 import '../styles/navbar.css';
 
 export default class Navbar extends React.Component {
     render() {
         return (
-                    <div className="navbar">
-                      <div className="navbar-image"><img src=""/></div>
-                      <div className="navbar-container">
-                        <div className="navbar-links-container">
-                          <a href="/about/vision" className="nav-links">About</a>
-                          <a href="/events/recent" className="nav-links">Events</a>
-                          <a href="/mou" className="nav-links">MOUs</a>
-                          <a href="/initiatives" className="nav-links">Initiatives</a>
-                          <a href="/awards" className="nav-links">Awards</a>
-                          <a href="/donate" className="nav-links">Donate</a>
-                          <a href="/contact" className="nav-links">Contact</a>
-                        </div>
-                        <div className="loginbutton">Login</div>
+                  <div className="navbar">
+                    <Link to="/"><img src="/static/images/favicon.png" className="navbar-image"/></Link>
+                    <div className="navbar-container">
+                      <div className="navbar-links-container">
+                        <Link to="/about/vision" className="nav-links">About</Link>
+                        <Link to="/events/recent" className="nav-links">Events</Link>
+                        <Link to="/mou" className="nav-links">MOUs</Link>
+                        <Link to="/initiatives" className="nav-links">Initiatives</Link>
+                        <Link to="/awards" className="nav-links">Awards</Link>
+                        <Link to="/donate" className="nav-links">Donate</Link>
+                        <Link to="/contact" className="nav-links">Contact</Link>
                       </div>
+                      <div className="loginbutton">Login</div>
                     </div>
+                  </div>
                 );
     }
 }
