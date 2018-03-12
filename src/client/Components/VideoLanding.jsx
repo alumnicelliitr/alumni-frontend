afd1642596849b245df2c24ddec00938d42da06a
psinghal20 <psinghal20@gmail.com>
2018-03-12 23:16:07 +0530

    feat: add youtube i-frame and imac-template

--- src/client/Components/VideoLanding.jsx
+++ src/client/Components/VideoLanding.jsx
@@ -3,12 +3,15 @@ import '../styles/videoLanding.css';
 
 export default class VideoLanding extends React.Component {
   render() {
-    const content = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam sem justo, pulvinar convallis faucibus non, rhoncus vitae leo. Pellentesque ut est erat. Duis id ultrices nisi. Quisque ornare lorem ex, sit amet euismod tortor mollis at. Phasellus ullamcorper nibh nec vehicula volutpat. Donec lacinia sagittis lectus, maximus malesuada metus auctor in. Donec gravida dui odio, suscipit dignissim dui rutrum accumsan. Mauris sit amet nulla non nisl porttitor sagittis. Sed vestibulum consectetur convallis. Interdum et malesuada fames ac ante ipsum primis in faucibus. Aenean ullamcorper libero rhoncus mi pharetra iaculis. Praesent a lacinia arcu.'
+    const content = 'Lorem ipsum dolor sit amet, consect ut est erat. Duis id ultrices nisi. Quisque ornare lorem ex, sit amet euismod tortor mollis at. Phasellus ullamcorper nibh nec vehicula volutpat. Donec lacinia sagittis lectus, maximus malesuada metus auctor in. Donec gravida dui odio, suscipit dignissim dui rutrum accumsan. Mauris sit amet nulla non nisl porttitor sagittis. Sed vestibulum consectetur convallis. Interdum et malesuada fames ac ante ipsum primis in faucibus. Aenean ullamcorper libero rhoncus mi pharetra iaculis. Praesent a lacinia arcu.'
     return (
       <div>
         <div className="video-landing-container">
           <div className="container">
-            <img className="video-image" src="/static/images/grey.jpg"/>
+            <div className="iframe-container">
+              <img className="video-image" src="/static/images/imac-template.png"/>
+              <iframe className="iframe-video" src="https://www.youtube.com/embed/VeSApPYcCBM" frameBorder="0" allow="autoplay; encrypted-media" allowFullScreen></iframe>
+            </div>
             <div className="video-content">
               <h3 className="light-heading">Video<br/><span className="dark-subheading">Repository</span></h3>
               <p>{content}</p>
@@ -20,7 +23,7 @@ export default class VideoLanding extends React.Component {
             <div className="more-content">
               <h4 className="light-heading">More of Video Repository</h4>
               <span>Visit our YouTube Channel</span>
-              <a className="youtube-landing-button" href="#">YouTube channel</a>
+              <a className="youtube-landing-button" href="https://www.youtube.com/channel/UCodyYSNBkcFMrZbOUZ0qlBw">YouTube channel</a>
             </div>
           </div>
         </div>
--- src/client/Components/messageDora.jsx
+++ src/client/Components/messageDora.jsx
@@ -6,10 +6,12 @@ export default class MessageDora extends React.Component {
     return (
         <div className="message-dora">
           <div className="container">
-            <h3 className="light-heading">Messsage from<br/><span className="dark-subheading">DORA</span></h3>
-            <div className="message-dora-content">
-              <div className="message">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam sem justo, pulvinar convallis faucibus non, rhoncus vitae leo. Pellentesque ut est erat. Duis id ultrices nisi. Quisque ornare lorem ex, sit amet euismod tortor mollis at. Phasellus ullamcorper nibh nec vehicula volutpat. Donec lacinia sagittis lectus, maximus malesuada metus auctor in. Donec gravida dui odio, suscipit dignissim dui rutrum accumsan. Mauris sit amet nulla non nisl porttitor sagittis. Sed vestibulum consectetur convallis. Interdum et malesuada fames ac ante ipsum primis in faucibus. Aenean ullamcorper libero rhoncus mi pharetra iaculis. Praesent a lacinia arcu.</div>
-              <div className="message-video"></div>
+            <div className="message-dora-container">
+              <div className="message-dora-content">
+                <h3 className="light-heading">Messsage from<br/><span className="dark-subheading">DORA</span></h3>
+                <div className="message">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam sem justo, pulvinar convallis faucibus non, rhoncus vitae leo. Pellentesque ut est erat. Duis id ultrices nisi. Quisque ornare lorem ex, sit amet euismod tortor mollis at. Phasellus ullamcorper nibh nec vehicula volutpat. Donec lacinia sagittis lectus, maximus malesuada metus auctor in. Donec gravida dui odio, suscipit dignissim dui rutrum accumsan. Mauris sit amet nulla non nisl porttitor sagittis. Sed vestibulum consectetur convallis. Interdum et malesuada fames ac ante ipsum primis in faucibus. Aenean ullamcorper libero rhoncus mi pharetra iaculis. Praesent a lacinia arcu.</div>
+              </div>
+              <iframe className="message-video" src="https://www.youtube.com/embed/VeSApPYcCBM" frameBorder="0" allow="autoplay; encrypted-media" allowFullScreen></iframe>
             </div>
           </div>
         </div>
