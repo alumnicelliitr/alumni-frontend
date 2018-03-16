e5d81dcaec85a444ca2acd33592ab3546d0ae077
psinghal20 <psinghal20@gmail.com>
2018-03-17 02:40:31 +0530

    feat: add donate and awards component

--- src/client/Containers/awards.jsx
+++ src/client/Containers/awards.jsx
@@ -1,47 +1,49 @@
 import React from 'react';
-import AwardsCard from '../Components/awardsCard.jsx';
+import ToggleRow from '../Components/toggleRow.jsx';
 import Navbar from '../Components/navbar.jsx';
 import LandingFooter from '../Components/landingFooter.jsx';
+import { baseUrl } from '../config.js';
 
 import '../styles/awards.css';
 
 export default class Awards extends React.Component {
-  render() {
-    const content = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam sem justo, p dignissim dui rutrum accumsan. Mauris sit amet nulla non nisl porttitor sagittis. Sed vestibulum consectetur convallis. Interdum et malesuada fames ac ante ipsum primis in faucibus. Aenean ullamcorper libero rhoncus mi pharetra iaculis. Praesent a lacinia arcu.";
-    const cardsContent = [
-      {
-        title: 'Encore Awards',
-        content: content
-      },
-      {
-        title: 'Heritage Awards',
-        content: content
-      },
-      {
-        title: 'Ram Kumar Prize',
-        content: content
-      },
-      {
-        title: 'Manoj Jain Award',
-        content: content
-      },
-      {
-        title: 'Celebrating Life Awards',
-        content: content
-      },
-
-    ];
-    const cards = cardsContent.map((card,index) => {
-      return (
-        <AwardsCard title={card.title} content={card.content} key={index} />
-      );
+  constructor(props){
+    super(props);
+    this.state = {
+      isLoaded:false,
+      data: {},
+    }
+  }
+  componentDidMount(){
+    let request = new Request(`${baseUrl}/api/website/awards/`,{
+      method:'get',
     });
+    fetch(request)
+    .then((res) => res.json())
+    .then((res) => {
+      this.setState({
+        isLoaded: true,
+        data: res,
+      });
+    });
+  }
+  render() {
+    let rows;
+    if (this.state.isLoaded){
+      rows = this.state.data.map((row,index) => {
+        return (
+          <ToggleRow {...row}/>
+        );
+      });
+    }
     return (
       <div>
         <Navbar />
         <div className="awards-container">
-          <div className="container">
-            {cards}
+          {rows}
+          <div className="donate-button-container">
+            <p>Donate for an award</p>
+            <a className="donate-button" href="#">Donate</a>
           </div>
         </div>
         <LandingFooter/>
