e79a0af7d18de1758dbb13047caeda85c30567c6
subrat <=>
2018-03-17 14:12:31 +0530

    Fixed carousel and adds medium blog contents

--- src/client/Components/trendingNews.jsx
+++ src/client/Components/trendingNews.jsx
@@ -1,7 +1,7 @@
-import React from 'react';
-import '../styles/trendingNews.css';
-import { MEDIUM_RSS_URL, RSS_FEED_URL, API_DOMAIN } from '../../shared/config';
-import Loader from './loader.jsx';
+
+import React from 'react'
+import '../styles/trendingNews.css'
+import { MEDIUM_RSS_URL, RSS_FEED_URL, API_DOMAIN } from '../../shared/config'
 
 export default class TrendingNews extends React.Component {
   constructor(props) {
@@ -10,7 +10,9 @@ export default class TrendingNews extends React.Component {
       isLoaded: false,
       data: [],
     }
+    this.initializeCarousel = this.initializeCarousel.bind(this)
   }
+
 
   componentWillMount() {
     const request = new Request(`${API_DOMAIN}api/website/headlines/`, {
@@ -18,15 +20,14 @@ export default class TrendingNews extends React.Component {
     })
     fetch(`${MEDIUM_RSS_URL}?rss_url=${RSS_FEED_URL}`)
       .then(res => res.json())
-      .then((res) => {
+      .then((res) => {    
         this.setState({
-          isLoaded: false,
           data: res.items,
         })
       })
     fetch(request)
       .then(res => res.json())
-      .then((res) => {
+      .then((res) => {   
         const { data } =  this.state
         this.setState({
           isLoaded: true,
@@ -34,32 +35,64 @@ export default class TrendingNews extends React.Component {
         })
       })
   }
+  
+  componentDidMount() {
+    // $('.carousel').carousel()
+    this.initializeCarousel()
+    $('.movePrevCarousel').click((e) => {
+      e.preventDefault()
+      e.stopPropagation()
+      $('.carousel').carousel('prev')
+    })
+    
+    $('.moveNextCarousel').click((e) => {
+      e.preventDefault()
+      e.stopPropagation()
+      $('.carousel').carousel('next')
+    })
+  }
 
-  componentDidMount(){
-    $('.main-carousel').flickity({
-      cellAlign: 'left',
-      wrapAround: true,
-      autoPlay: true,
-    });
+  initializeCarousel() {
+    const slider = $('.carousel')
+    slider.carousel()
+    this.state.data.forEach((news) => {
+      if (news.id) {
+        slider.append(`<a class="carousel-item carousel-links">
+        <img class="image" src=${news.image} alt=${news.title}/>
+        <div class="image-heading"><h1>${news.title}</h1></div>
+      </a>`)
+      } else {
+        slider.append(`<a class="carousel-item carousel-links">
+        <img class="image" src=${news.thumbnail} alt=${news.title}/>
+        <div class="image-heading"><h1>${news.title}</h1></div>
+      </a>`)
+      }
+    })
+    this.reinitializeCarousel()
+  }
+
+  reinitializeCarousel() {
+    $('.carousel').carousel('destroy')
+    $('.carousel').carousel() 
   }
 
   render() {
-    if (this.state.isLoaded) {
-      this.state.data.map((slide) => {
-        const $cellElems = $(`<div class="carousel-cell">
-            <img class="carousel-image" src=${slide.thumbnail ? slide.thumbnail : '/static/images/grey.jpg'}/>
-            <h4 class="carousel-heading">${slide.title}</h4>
-          </div>`);
-        $('.main-carousel').flickity( 'append', $cellElems );
-      });
-    }
-
+    this.reinitializeCarousel()
     return (
       <div className="trending-news">
-        { !this.state.isLoaded ? (<Loader/>) : ''}
-        <div className="main-carousel">
+        <div className="carousel carousel-slider center" data-indicators="true">
+          <div className="carousel-fixed-item center middle-indicator">
+            <div className="left">
+              <a className="movePrevCarousel middle-indicator-text waves-effect waves-light content-indicator"><i className="material-icons left  middle-indicator-text">chevron_left</i></a>
+            </div>
+            <div className="right">
+              <a className=" moveNextCarousel middle-indicator-text waves-effect waves-light content-indicator"><i className="material-icons right middle-indicator-text">chevron_right</i></a>
+            </div>
+          </div>
+          {this.initializeCarousel()}
+          {this.reinitializeCarousel()}
         </div>
       </div>
-    );
+    )
   }
 }
