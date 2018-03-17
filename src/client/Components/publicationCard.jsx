9a8657d27878b8991a47ae8dec608a493c4e814a
psinghal20 <psinghal20@gmail.com>
2018-03-18 02:51:21 +0530

    feat: add api calling in the share your story

--- src/client/Components/publicationCard.jsx
+++ src/client/Components/publicationCard.jsx
@@ -5,9 +5,9 @@ export default class PublicationCard extends React.Component {
   render() {
     return (
       <div className="publication-card">
-        <img src="/static/images/grey.jpg" className="publication-card-image"/>
+        <img src={this.props.thumbnail} className="publication-card-image"/>
         <div className="publication-card-content">
-          Some Content
+          {this.props.title}
         </div>
       </div>
     );
