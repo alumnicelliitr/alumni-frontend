4494bbec8912fc4bc4795a85adf8bff65a637c42
psinghal20 <psinghal20@gmail.com>
2018-03-18 11:10:13 +0530

    feat: add write form in publications

--- src/client/Containers/publications.jsx
+++ src/client/Containers/publications.jsx
@@ -11,12 +11,49 @@ export default class Publications extends React.Component {
     super(props);
     this.state = {
       activeTab:0,
+      writeMode: false,
+      story: '',
+      file: {},
     };
   }
 
   tabClick(clickTab) {
     this.setState({
-      activeTab: clickTab
+      activeTab: clickTab,
+      writeMode: this.state.writeMode,
+      story: this.state.story,
+      file: this.state.file,
+    });
+  }
+
+  writeClick() {
+    this.setState({
+      activeTab: this.state.activeTab,
+      writeMode: !this.state.writeMode,
+      story: this.state.story,
+      file: this.state.file,
+    });
+  }
+
+  readURL(input) {
+    if (input.files && input.files[0]) {
+        var reader = new FileReader();
+
+        reader.onload = function (e) {
+            $('#image-display')
+                .attr('src', e.target.result)
+        };
+
+        reader.readAsDataURL(input.files[0]);
+    }
+  }
+
+  storyChange(e) {
+    this.setState({
+      activeTab: this.state.activeTab,
+      writeMode: this.state.writeMode,
+      story: e.target.value,
+      file: this.state.file,
     });
   }
 
@@ -31,6 +68,28 @@ export default class Publications extends React.Component {
       ( <div id="share-your-story" className="publications-containers"><ShareYourStory /></div> ),
     ];
 
+    const writeContent = (
+      <div className="write-form-container">
+        <div className="form-outer">
+          <div className="upload-image">
+            <img id="image-display" />
+            <input type='file' className="inputfile" onChange={(e) => this.readURL(e.target)} />
+          </div>
+          <div className="write-story">
+            <textarea name="write-story" className="browser-default" value={this.state.story} onChange={(e) => this.storyChange(e)}></textarea>
+          </div>
+        </div>
+        <div className="button-container">
+          <div className="cancel-button">Cancel</div>
+          <div className="submit-button">Submit</div>
+        </div>
+      </div>
+    );
+
+    let content = tabContent[this.state.activeTab];
+    if(this.state.writeMode) {
+      content = writeContent;
+    }
     return (
       <div>
       <Navbar/>
@@ -38,7 +97,8 @@ export default class Publications extends React.Component {
         <div>
           <Tabs tabs={tabs} onClick={(clickTab) => this.tabClick(clickTab)} activeTab={this.state.activeTab} />
         </div>
-        {tabContent[this.state.activeTab]}
+        <div className="write-button" onClick={(e) => this.writeClick()}><img src="/static/images/signature.svg"/>Write</div>
+        {content}
       </div>
       <LandingFooter/>
       </div>
