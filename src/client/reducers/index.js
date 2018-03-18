667aef4cd036608c0ad93a857beead76f29310d2
psinghal20 <psinghal20@gmail.com>
2018-03-18 11:03:03 +0530

    feat: add user reducer and actions

--- src/client/reducers/index.js
+++ src/client/reducers/index.js
@@ -1,7 +1,8 @@
-import { combineReducers } from 'redux'
+import { combineReducers } from 'redux';
+import user from './user.js';
 
 const rootReducer = combineReducers({
+  user,
+});
 
-})
-
-export default rootReducer
+export default rootReducer;
