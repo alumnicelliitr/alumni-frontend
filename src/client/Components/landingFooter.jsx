a90bbce297a4f652ca56ca26a06fb01748f9f058
psinghal20 <psinghal20@gmail.com>
2018-03-20 01:52:21 +0530

    fix: add links and social link for medium

--- src/client/Components/landingFooter.jsx
+++ src/client/Components/landingFooter.jsx
@@ -2,31 +2,53 @@ import React from 'react';
 import '../styles/landingFooter.css';
 
 export default class LandingFooter extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoaded: false,
+      data: [],
+    }
+  }
+
+  componentDidMount() {
+    let request = new Request('https://institute-alumni-relation-cell.herokuapp.com/api/website/events/',{
+      method:'get',
+    });
+    fetch(request)
+    .then((res) => res.json())
+    .then((res) => {
+      this.setState({
+        isLoaded: true,
+        data: res,
+      });
+    });
+  }
+
   render() {
+    const events = this.state.data.map((obj) => { return (<a className="subheading-link" href="#">{Object.keys(obj)[0]}</a>) });
+
     return (
       <div className="landing-footer container">
         <div className="links-list">
-          <a className="heading-link" href="#">Home</a>
-          <a className="heading-link" href="#">About Us</a>
+          <a className="heading-link" href="/">Home</a>
+          <a className="heading-link" href="about/vision">About Us</a>
           <a className="heading-link" href="#">Student Affairs</a>
         </div>
         <div className="links-list">
-          <a className="heading-link" href="#">Initiatives</a>
-          <a className="subheading-link" href="#">SAMP</a>
-          <a className="subheading-link" href="#">COR</a>
-          <a className="subheading-link" href="#">Video Repository</a>
+          <a className="heading-link" href="/initiatives">Initiatives</a>
+          <a className="subheading-link" href="/initiatives">SAMP</a>
+          <a className="subheading-link" href="/initiatives">COR</a>
+          <a className="subheading-link" href="/initiatives">Video Repository</a>
         </div>
         <div className="links-list">
           <a className="heading-link" href="#">Blogs</a>
-          <a className="subheading-link" href="#">Know Your Alum</a>
-          <a className="subheading-link" href="#">Share Your Story</a>
+          <a className="subheading-link" href="/publications">Know Your Alum</a>
+          <a className="subheading-link" href="/publications">Share Your Story</a>
+          <a className="subheading-link" href="https://medium.com/institute-alumni-relations-cell-iit-roorkee">Medium</a>
         </div>
         <div className="links-list">
           <a className="heading-link" href="#">Events</a>
-          <a className="subheading-link" href="#">Golden Jubliee</a>
-          <a className="subheading-link" href="#">Silver Jubliee</a>
-          <a className="subheading-link" href="#">Ruby Jubliee</a>
-          <a className="subheading-link" href="#">Alumni Meets</a>
+          {events}
         </div>
         <div className="links-list">
           <a className="heading-link" href="#">Contact</a>
@@ -39,6 +61,7 @@ export default class LandingFooter extends React.Component {
             <a href="https://www.facebook.com/alumnicelliitr/"><img src="/static/images/fb-dark.svg" className="follow-icon"/></a>
             <a href="https://www.linkedin.com/company/iarciitr"><img src="/static/images/in-dark.svg" className="follow-icon"/></a>
             <a href="https://www.youtube.com/channel/UCodyYSNBkcFMrZbOUZ0qlBw"><img src="/static/images/youtube-dark.svg" className="follow-icon"/></a>
+            <a href="https://medium.com/institute-alumni-relations-cell-iit-roorkee"><img src="/static/images/Monogram.svg" className="follow-icon"/></a>
           </div>
         </div>
       </div>
