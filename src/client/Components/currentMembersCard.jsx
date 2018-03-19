f0f665b6be5ba05866af6158e91a2835e5b6a560
psinghal20 <psinghal20@gmail.com>
2018-03-20 01:50:46 +0530

    fix: add role and fix styling

--- src/client/Components/currentMembersCard.jsx
+++ src/client/Components/currentMembersCard.jsx
@@ -5,13 +5,14 @@ export default class CurrentMemberCard extends React.Component {
   render() {
     return (
         <div className="current-members-card">
-          <img src={this.props.image} className="current-members-card-image"/>
+          <img src={'/static/images/grey.jpg'} className="current-members-card-image"/>
           <div className="current-members-card-content">
+            <div className="member-role">{this.props.team.role}</div>
             <div className="members-info">
               <div><a href={this.props.socials[0] || 'https://twitter.com/alumnicelliitr'}><img className="image" src="/static/images/twitter-icon.svg"/></a>{this.props.name}</div>
               <div><a href={this.props.socials[0] || 'https://twitter.com/alumnicelliitr'}><img className="image" src="/static/images/fb-icon.svg"/></a>{this.props.branch}</div>
+              <div className="vertical-seperator"/>
             </div>
-            <div className="vertical-seperator"/>
           </div>
         </div>
       );
