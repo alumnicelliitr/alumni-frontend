bdfdc841e3c1c04fa08b53b4e584d8c8275db619
psinghal20 <psinghal20@gmail.com>
2018-03-20 01:49:34 +0530

    feat: add mou card component and styles

---
+++ src/client/Components/mouCard.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import '../styles/mouCard.css';
+
+export default class MOUCard extends React.Component {
+  render() {
+    return (
+        <div className="mou-card">
+          <div>
+            <h3 className="light-heading">{this.props.lightHeading}<br/><span className="dark-subheading">{this.props.darkHeading}</span></h3>
+            <div className="mou-card-content">{this.props.content}</div>
+          </div>
+          <img src="/static/images/grey.jpg" className="mou-card-image"/>
+        </div>
+      );
+  }
+}
